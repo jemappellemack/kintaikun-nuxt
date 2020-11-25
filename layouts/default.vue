32980db57665256e9f523a05cce5e22f784ec053
hayakawa.makoto <jemappellemack@gmail.com>
2020-11-25 09:21:57 +0900

    feat(layout): implement compositions

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, provide, SetupContext } from '@nuxtjs/composition-api'
+import { useGlobalState, GlobalStateKey } from '@/compositions'
+
+export default defineComponent({
+  setup(props: any, { root }: SetupContext) {
+    provide(GlobalStateKey, useGlobalState())
+  }
+})
+</script>
