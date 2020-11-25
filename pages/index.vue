130b0fdd90c07f132b268c1d85b31351763cfa42
hayakawa.makoto <jemappellemack@gmail.com>
2020-11-25 09:25:02 +0900

    feat(index): create page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navigation/>
+    <div class="container">
+      <h1>ホーム画面</h1>
+      <p>
+        <NuxtLink to="/timestamp">
+          打刻画面
+        </NuxtLink>
+      </p>
+      <p>
+        <NuxtLink to="/works">
+          一覧画面
+        </NuxtLink>
+      </p>
+    </div>
+  </div>
+</template>
